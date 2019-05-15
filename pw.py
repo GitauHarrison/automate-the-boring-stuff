bf309c29911c222fd7b4cc01a43092f283b09bb9
Gitau Harrison <harrisongitau3@gmail.com>
2019-05-15 20:41:53 +0300

    Check whether the account name exists in the dictionary

--- pw.py
+++ pw.py
@@ -9,10 +9,17 @@ passwords = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
             'luggage': '12345'
 }
 
-import sys
+import sys, pyperclip
 
 if len(sys.argv)<2:
     print('Usage: python pw.py [account] - copy account password')
     sys.exit()
 
 account = sys.argv[1] # first command line arg is the account name
+
+#check if account name exists in the dictionary
+if account in passwords: 
+    pyperclip.copy(passwords[account])
+    print('Passwords for ' + account + ' copied to clipboard.')
+else:
+    print('There is no account named ' + account)
