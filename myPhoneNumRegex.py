d194b058b5dc0eb3dc3f0a789b396582ff60d884
Gitau Harrison <harrisongitau3@gmail.com>
2019-06-22 06:14:07 +0300

    Add more regular expressions

--- myPhoneNumRegex.py
+++ myPhoneNumRegex.py
@@ -10,3 +10,24 @@ mo = phoneNumRegex.search('My number is 435-555-4242')
 
 #return matched object using group() method
 print('Phone number found: ' + mo.group())
+
+
+##-----------------
+# Using a | (pipe)
+##-----------------
+
+heroRegex = re.compile(r'Batman | Tina Fey')
+mo1 = heroRegex.search('Batman and Tina Fey')
+mo1.group() #returns the first occurance of the matching text
+
+#Multiple prefixes
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+mo2 = batRegex.search('Batmobile lost a wheel')
+mo2.group() #returns Batman
+
+##-------------------------
+#Optional group using ? (Zero or 1 match)
+##-------------------------
+myBatRegex = re.compile(r'Bat(wo)?man')
+mo3 = myBatRegex.search('The adventures of Batman')
+mo3.group() #returns Batman... (wo) group was optional
