a27531da78ac63591a4299677efa65e49db27e1f
Gitau Harrison <harrisongitau3@gmail.com>
2019-10-15 05:11:09 +0300

    Add the file errorInfo.txt to capture the exception as a string

--- debugging/boxaPrint.py
+++ debugging/boxaPrint.py
@@ -1,4 +1,4 @@
-def boxPrint(symbol, width,height):
+def boxPrint(symbol, width, height):
     if len(symbol) != 1:
         raise Exception('Symbol must be a single character string.')
     if width <= 2: 
