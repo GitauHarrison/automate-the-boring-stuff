a73135fdbedcd38337a99e7a8c48c4cbda612a14
Gitau Harrison <harrisongitau3@gmail.com>
2019-10-18 04:26:53 +0300

    Add factorialLog.py file to learn how to log record the file

--- debugging/errorExample.py
+++ debugging/errorExample.py
@@ -5,3 +5,14 @@ def bacon():
 
 spam()
 
+# Obtaining an exception as a string on the interpreter
+
+
+#import traceback
+#try:
+         #raise Exception('This is the error message.')
+#except:
+         #errorFile = open('errorInfo.txt', 'w')
+         #errorFile.write(traceback.format_exc())
+         #errorFile.close()
+         #print('The traceback info was written to errorInfo.txt.')
