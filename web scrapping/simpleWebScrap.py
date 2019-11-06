15af4f792f4d562ca6b6d641a2f188c105249856
Gitau Harrison <harrisongitau3@gmail.com>
2019-11-06 12:02:07 +0300

    Print all links to terminal

--- web scrapping/simpleWebScrap.py
+++ web scrapping/simpleWebScrap.py
@@ -1,7 +1,7 @@
 #! python3
 
 #TO-DO 1: Import all the necessary modules
-import requests, bs4, time
+import requests, bs4, time, webbrowser
 
 # TO-DO 2: Download a html page using requests
 req = requests.get('https://chezalab.netlify.com/')
@@ -17,5 +17,14 @@ soup = bs4.BeautifulSoup(req.text, 'html.parser')
 
 #TO-DO 4: Print relevant info on the terminal
 print(soup.find_all('p')[3].get_text())
+
+time.sleep(2)
+
 print(soup.find_all(class_='feature-title')[0].get_text())
-#TO-DO 5: Open links from the html page on new tabs using webbrowser module
+#TO-DO 5: Open links from the html page on new tabs using webbrowser 
+
+time.sleep(2)
+
+for link in soup.find_all('a'):
+    #webbrowser.open_new_tab(link.get('href'))
+    print(link.get('href'))
