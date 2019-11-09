b5335a0ceaa5aff6212d1ec40461e9dfe3e50419
Gitau Harrison <harrisongitau3@gmail.com>
2019-11-09 14:31:01 +0300

    Add logInToGmail.py to log a user into his/her GMAIL account then log them out after a while

---
+++ web scrapping/logInToGmail.py
@@ -0,0 +1,35 @@
+#! python3
+
+# logInToGmail.py - logs you into your GMAIL account,
+# then log you out after a short while
+
+
+# TO-DO 1: Import all the needed modules
+
+# TO-DO 2: Choose the browser to use
+
+# TO-DO 3: Create a URL link to GMAIL
+
+# TO-DO 4: Find the element to the email address
+
+# TO-DO 5: Fill in the email address
+
+# TO-DO 6: Click Next
+
+# TO-DO 7: Find the element to the password box
+
+# TO-DO 8: Fill in the password
+
+# TO-DO 9: Click Next
+
+#--------------------------
+#SIGNING OUT
+#--------------------------
+
+# TO-DO 10: Find the element to the user profile
+
+# TO-DO 11: Click the user profile
+
+# TO-DO 12: Find the element to the sign out text link
+
+# TO-DO 13: Sign out
