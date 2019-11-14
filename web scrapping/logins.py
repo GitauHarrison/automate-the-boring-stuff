4ec8495e4d4d51ad32c810f6d83a2df5c0e2bc2b
Gitau Harrison <harrisongitau3@gmail.com>
2019-11-14 06:50:25 +0300

    Ad d logins.py file which has the ability to open, focus and close  multiple tabs for a user

---
+++ web scrapping/logins.py
@@ -0,0 +1,103 @@
+#! python3
+# logins.py - logs user to accounts on multiple tabs using selenium
+
+# TO-DO 1: Log into GMAIL in the default tab
+
+# TO-Do 2: Open a new tab
+
+# TO-DO 3: Switch to the new tab
+
+# TO-DO 4: Log into Slack
+
+# TO-DO 5: Open a new tab
+
+# TO-DO 6: Switch to the new tab
+
+# TO-DO 7: Log into trello
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+browser = webdriver.Chrome()
+browser.maximize_window()
+
+#--------------
+# TAB 1: GMAIL
+# -------------
+def gmailLogin():
+    browser.get('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=3&cad=rja&uact=8&ved=2ahUKEwjPyM2w_NzlAhXZad4KHenwBXcQFjACegQIARAB&url=https%3A%2F%2Faccounts.google.com%2Fb%2F1%2FAddMailService&usg=AOvVaw0XvudbS8_frp7iekcbIOHZ')
+    emailElem = browser.find_element_by_class_name('whsOnd')
+    emailElem.clear()
+    emailElem.send_keys('myEmail')
+    emailNextButtonElem = browser.find_element_by_class_name('RveJvd')
+    emailNextButtonElem.click()
+    time.sleep(1.5)
+    passwordElem = browser.find_element_by_class_name('zHQkBf')
+    passwordElem.clear()
+    passwordElem.send_keys('myPassword')
+    passwordNextElem = browser.find_element_by_class_name('CwaK9')
+    passwordNextElem.click()
+    time.sleep(3)
+
+#--------------
+# TAB 2: SLACK
+#--------------
+def slackLogin():
+    browser.get('https://tinkeredu.slack.com')
+    slackEmailElem = browser.find_element_by_id('email')
+    slackEmailElem.clear()
+    slackEmailElem.send_keys('myEmail')
+    slackPasswordElem = browser.find_element_by_id('password')
+    slackPasswordElem.clear()
+    slackPasswordElem.send_keys('myPassword')
+    signInElem = browser.find_element_by_id('signin_btn').submit()
+    time.sleep(5)
+
+
+
+#--------------
+# TAB 2: TRELLO
+#--------------
+def trelloLogin():
+    browser.get('https://trello.com/login')
+    loginWithGmailElem = browser.find_element_by_id('google-link')
+    loginWithGmailElem.click()
+    time.sleep(5)
+
+
+
+
+
+gmailLogin()
+
+#------Switching tabs--------
+
+browser.execute_script("window.open('');")
+browser.switch_to_window(browser.window_handles[1])
+time.sleep(1.5)
+
+slackLogin()
+
+#------Switching tabs--------
+
+browser.execute_script("window.open('');")
+browser.switch_to_window(browser.window_handles[2])
+time.sleep(1.5)
+
+trelloLogin()
+
+#------ Switching thoughth the tabs --------
+for tabs in range(2):
+    browser.switch_to_window(browser.window_handles[0])
+    time.sleep(1.5)
+    browser.switch_to_window(browser.window_handles[1])
+    time.sleep(1.5)
+    browser.switch_to_window(browser.window_handles[2])
+
+
+time.sleep(3)
+browser.quit()
+
