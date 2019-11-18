c830957a6e0f5ba74e8a309baf705e96e148cfef
Gitau Harrison <harrisongitau3@gmail.com>
2019-11-18 10:39:06 +0300

    Take screenshots when account is logged in

--- web scrapping/logins.py
+++ web scrapping/logins.py
@@ -31,16 +31,17 @@ def gmailLogin():
     browser.get('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=3&cad=rja&uact=8&ved=2ahUKEwjPyM2w_NzlAhXZad4KHenwBXcQFjACegQIARAB&url=https%3A%2F%2Faccounts.google.com%2Fb%2F1%2FAddMailService&usg=AOvVaw0XvudbS8_frp7iekcbIOHZ')
     emailElem = browser.find_element_by_class_name('whsOnd')
     emailElem.clear()
-    emailElem.send_keys('myEmail')
+    emailElem.send_keys('harrison@tinkeredu.net')
     emailNextButtonElem = browser.find_element_by_class_name('RveJvd')
     emailNextButtonElem.click()
     time.sleep(1.5)
     passwordElem = browser.find_element_by_class_name('zHQkBf')
     passwordElem.clear()
-    passwordElem.send_keys('myPassword')
+    passwordElem.send_keys('S!mps0n@24..,,')
     passwordNextElem = browser.find_element_by_class_name('CwaK9')
     passwordNextElem.click()
     time.sleep(3)
+    browser.save_screenshot('gmail.png')
 
 #--------------
 # TAB 2: SLACK
@@ -49,12 +50,13 @@ def slackLogin():
     browser.get('https://tinkeredu.slack.com')
     slackEmailElem = browser.find_element_by_id('email')
     slackEmailElem.clear()
-    slackEmailElem.send_keys('myEmail')
+    slackEmailElem.send_keys('harrison@tinkeredu.net')
     slackPasswordElem = browser.find_element_by_id('password')
     slackPasswordElem.clear()
-    slackPasswordElem.send_keys('myPassword')
+    slackPasswordElem.send_keys('Turf2013W!n..')
     signInElem = browser.find_element_by_id('signin_btn').submit()
     time.sleep(5)
+    browser.save_screenshot('slacksignin.png')
 
 
 
@@ -66,6 +68,7 @@ def trelloLogin():
     loginWithGmailElem = browser.find_element_by_id('google-link')
     loginWithGmailElem.click()
     time.sleep(5)
+    browser.save_screenshot('trellologin.png')
 
 
 
@@ -89,7 +92,7 @@ time.sleep(1.5)
 
 trelloLogin()
 
-#------ Switching thoughth the tabs --------
+#------ Switching through the tabs --------
 for tabs in range(2):
     browser.switch_to_window(browser.window_handles[0])
     time.sleep(1.5)
